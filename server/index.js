a1ecd77b94ea4452b23f4502f08834b9931c5402
Abhi-nolyfe <abhishek.tyagi912001@gmail.com>
2023-03-15 03:08:13 +0530

    reorganized connection to mongodb

--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import productRoutes from './routes/products.js';
+import connectDB from './mongodb/connect.js';
 
+dotenv.config();
 
 const app = express();
-
-app.use(bodyParser.json({limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true }));
 app.use(cors());
+app.use(express.json({ limit: "30mb" }));
+app.use('/products', productRoutes);
 
-// connecting to mongodb.
-const CONNECTION_URL = 'mongodb+srv://mern:mernpassword@cluster0.vpkeihw.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World!' });
+});
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+const startServer = async () => {
+    try {
+        // connect to the database...
+        connectDB(process.env.MONGODB_URL);
 
-mongoose.set();
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+startServer();
