cc12ddfda1bbd55a753df13a36f94f0718abae88
Abhi-nolyfe <abhishek.tyagi912001@gmail.com>
2023-03-21 00:17:18 +0530

    added code to get data product data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
-
 import { Product, FooterBanner, HeroBanner } from './components'
+import { fetchProducts } from './api'
 
-const Home = () => {
+const Home = ({ products }) => {
     return (
         <>
             <HeroBanner />
 
             <div className="products-heading">
                 <h2>Best Selling Products</h2>
-                <p>Speakers of many variations</p>
+                <p>list of best selling products</p>
             </div>
 
             <div className="products-container">
-                {['Product 1', 'Product 2'].map(
-                (product) => product)}
+      {/* {passing products containing data to product} */}
+                {products?.map((product) => 
+                <Product key={product._id} product={product} />)}
             </div>
 
             <FooterBanner />
@@ -22,4 +23,21 @@ const Home = () => {
     );
 }
 
-export default Home;
+// fetching product data from fetchProduct api call and storing it in prop 'products' to use in home elements.
+
+const HomePage = () => {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchProductsData = async () => {
+      const { data } = await fetchProducts();
+      setProducts(data);
+    };
+
+    fetchProductsData();
+  }, []);
+
+  return <Home products={products} />;
+};
+
+export default HomePage;
