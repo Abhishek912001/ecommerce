d777f562e61dd0da6ff1c2ecc600b6dcb9c2b548
Abhi-nolyfe <abhishek.tyagi912001@gmail.com>
2023-03-21 00:17:47 +0530

    applied layout

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import Home from './App';
 import './styles/globals.css';
+import { Layout } from './components';
 
-createRoot(document.getElementById('root')).render(<Home />);
+createRoot(document.getElementById('root')).render(
+ <Layout>
+    <Home />
+ </Layout>);
