9569e34dbc0a05f1026634a864285a841e505461
abhi.nolye <abhi.nolye@gmail.com>
2023-04-03 16:27:06 +0530

    added statecontext

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client';
 
 import Home from './App';
 import './styles/globals.css';
 import { Layout } from './components';
 import { BrowserRouter as Router } from "react-router-dom";
+import { StateContext } from './context/StateContext';
+
 
 createRoot(document.getElementById('root')).render(
 <Router>
- <Layout>
-    <Home />
- </Layout>
-</Router>);
+   <StateContext>
+      <Layout>
+         <Toaster />
+         <Home />
+      </Layout>
+   </StateContext>
+</Router>
+);
