3f0728e4a65855f767bf0f8262598666ce27f9a0
abhi.nolye <abhi.nolye@gmail.com>
2023-04-22 20:58:52 +0530

    minor change

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client';
 
-import Home from './App';
+import Home from './pages/homePage';
 import './styles/globals.css';
 import { Layout } from './components';
 import { BrowserRouter as Router } from "react-router-dom";
