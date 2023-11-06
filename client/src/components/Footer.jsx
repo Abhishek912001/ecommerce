2210130bcba849db5fcbabd229686525d6b3c9fb
Abhi <abhi.nolyfe@gmail.com>
2023-11-06 11:47:30 +0530

    minor fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { AiFillGithub, AiFillLike } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillLike } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>Fully dynamic ecommerce website project built using mern stack.</p>
-      <p className="icons">
-      <a href="https://github.com/abhi-nolyfe/ecommerce-site" title="github" target="_blank" rel="noopener noreferrer">
+      <p>
+        Fully dynamic ecommerce website project built using mern stack.
+      </p>
+      <div className="icons">
+        <a
+          href="https://github.com/abhi-nolyfe/ecommerce-site"
+          title="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <AiFillGithub />
         </a>
         <AiFillLike />
-      </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
